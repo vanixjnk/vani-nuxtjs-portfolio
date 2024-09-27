76170bb8abd2a331ad39e9cff1e147275f20db61
vanixjnk <mnb.vpbq@gmail.com>
2024-09-27 12:24:31 +0700

    vani

---
+++ pages/contact.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="content-vh">
+        <div class="content">
+            <a-typography-title style="font-weight: bold; font-size: 40px;">
+                Let's Get in Touch:
+                <span style="color: #1677FF;">Ways to Connect with Me</span>
+            </a-typography-title>
+            <a-typography-paragraph>
+                Cảm ơn bạn đã quan tâm đến việc liên hệ với tôi. Tôi hoan nghênh phản hồi, câu hỏi và đề xuất của bạn. Nếu bạn có câu hỏi hoặc nhận xét cụ thể, vui lòng gửi email trực tiếp cho tôi theo địa chỉ <span style="color: #1677FF">{{ content.email }}</span>. Tôi cố gắng trả lời tất cả tin nhắn trong vòng 24 giờ, mặc dù tôi có thể mất nhiều thời gian hơn trong thời gian bận rộn. Cuối cùng, nếu bạn muốn kết nối trên mạng xã hội, bạn có thể kết nối với tôi bằng các liên kết mạng xã hội bên dưới.
+            </a-typography-paragraph>
+
+            <a :href="content.social_links.facebook" target="_blank">
+                <a-button type="text">
+                    <facebook-outlined /> Follow on Facebook
+                </a-button>
+            </a>
+
+            <br />
+
+            <a :href="content.social_links.instagram" target="_blank">
+                <a-button type="text">
+                    <instagram-outlined /> Follow on Instagram
+                </a-button>
+            </a>
+
+            <br />
+
+            <a :href="content.social_links.twitter" target="_blank">
+                <a-button type="text">
+                    <twitter-outlined /> Follow on Twitter
+                </a-button>
+            </a>
+
+            <br />
+
+            <a-divider></a-divider>
+
+            <a :href="'mailto:' + content.email">
+                <a-button type="text">
+                    <mail-outlined /> vanixjnk@gmail.com
+                </a-button>
+            </a>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import content from '~/static/content.json';
+
+useHead({
+    title: "Contact",
+})
+</script>
+
+<style scoped>
+.ant-typography {
+    font-size: 16px;
+}
+
+.ant-btn {
+    font-size: 16px;
+}
+
+.anticon {
+    color: #1677FF;
+}
+
+.content {
+    width: 100%;
+}
+
+/* lg */
+@media (min-width: 1024px) {
+    .content {
+        width: 80%;
+    }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="content-vh">
+        <a-flex gap="50" horizontal justify="center" align="center">
+            <div class="profile-info">
+
+                <!-- Info -->
+                <a-typography-title :level="4" style="margin: 0">
+                    {{ content.role }}
+                </a-typography-title>
+                <a-typography-title :level="1" style=" margin: 0 0 10px 0; font-weight: bold;">
+                    {{ content.name }}
+                </a-typography-title>
+                <a-typography-paragraph align="justify" style="font-size: 16px; color: #71717a;">
+                    {{ content.description }}
+                </a-typography-paragraph>
+
+                <!-- Buttons -->
+                <a-flex gap="8" style="margin-top: 25px;">
+                    <NuxtLink to="/about">
+                        <a-button type="primary">More About Me
+                            <RightOutlined />
+                        </a-button>
+                    </NuxtLink>
+                </a-flex>
+            </div>
+        </a-flex>
+    </div>
+</template>
+
+<script setup lang="ts">
+import content from '~/static/content.json';
+</script>
+
+<style scoped>
+.profile-image {
+    display: none;
+    rotate: 4deg;
+}
+
+.sm-profile-image {
+    border-radius: 100%;
+    margin-bottom: 18px;
+}
+
+:deep(.ant-image-mask) {
+    border-radius: 15px;
+}
+
+.content-vh {
+    padding-top: 85px !important;
+}
+
+/* lg */
+@media (min-width: 768px) {
+    .profile-info {
+        width: 60%;
+    }
+
+    .profile-image {
+        display: block;
+    }
+
+    .sm-profile-image {
+        display: none;
+    }
+
+    .content-vh {
+        padding-top: 0 !important;
+    }
+}
+</style>
